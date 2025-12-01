b369d539a84f52cb434e7bb21ab1e1a0621a92fe
Cesar Duarte <0991311@MY.SCCCD.EDU>
2025-12-01 15:08:12 -0800

    Struct to store Pokemon Cards

---
+++ Course Project/Course_Project.cpp
@@ -0,0 +1,31 @@
+// CSCI - 40 Final Project
+// Author: [Cesar Duarte]
+
+/*
+ * Description: I will make an inventory sysyem for Pokemon trading cards, or records with name, category, value, 
+ and condition.Users can add new items,
+ remove sold/lost items, search by name, and sort by value.
+ Data should persist between runs.
+ *
+*/
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+const int MAX_SIZE = 50;
+
+struct PokemonCard {
+    string name;
+    string type;
+    int cardNumber;
+    int hp;
+    float cost;
+};
+
+int main() {
+
+    return 0;
+}
