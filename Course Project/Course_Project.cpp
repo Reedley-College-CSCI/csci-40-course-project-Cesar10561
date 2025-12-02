be4b868fa418adffafaf7d96fe59afe2a431d770
Cesar Duarte <0991311@MY.SCCCD.EDU>
2025-12-01 16:07:34 -0800

    Used if and else statments to give the user options.

--- Course Project/Course_Project.cpp
+++ Course Project/Course_Project.cpp
@@ -21,18 +21,18 @@ const int MAX_SIZE = 50;
 struct PokemonCard {
     string name;
     string type;
-    int cardNumber;
     int hp;
     float cost;
 };
 
+
 // Function Prototypes
-void addCard(PokemonCard cards[], int &count);
-void displayCards(const PokemonCard cards[], int count);
-int linearSearch(const PokemonCard cards[], int count, string name);
-void bubbleSort(const PokemonCard cards[], int count);
-void removeCard(const PokemonCard cards[], int& count);
-void displayMenu();
+void addCard(PokemonCard cards[], int &count); // add cards
+void displayCards(const PokemonCard cards[], int count); // displays cards
+int linearSearch(const PokemonCard cards[], int count, string name); // searches the cards
+void bubbleSort(const PokemonCard cards[], int count); // sorts them from cost
+void removeCard(const PokemonCard cards[], int& count); // removes cards
+void displayMenu(); // displays the main menu
 
 
 
@@ -41,6 +41,44 @@ int main() {
     int count = 0;
     int choice = 1;
 
+    if (choice == 1) {
+        addCard(cards, count);
+    }
+    else if (choice == 2) {
+        displayCards(cards, count);
+    }
+    else if (choice == 3) {
+        cout << "Enter name to Search: ";
+ 
+    string name;
+    getline(cin, name);
+    int index = linearSearch(cards, count, name);
+
+    if (choice == -1) 
+        cout << "Card not found." << endl;
+    
+    else 
+        cout << "\nFound: " << cards[index].name << " | "
+            << cards[index].type << " | $"
+            << cards[index].cost << " | "
+            << cards[index].hp << endl;
+    }
+    else if (choice == 4) {
+        bubbleSort(cards, count);
+        cout << "Cards sorted!";
+    }
+    else if (choice == 5) {
+        removeCard(cards, count);
+    }
+    else if (choice == 6) {
+        cout << "Exiting Program....." << endl;
+    }
+    else
+        cout << "Invalid Option" << endl;
+
+
 
     return 0;
 }
+
+
